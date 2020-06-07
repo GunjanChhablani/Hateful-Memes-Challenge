Fixed CrossEntropy and Softmax

core/modules/metrics.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import roc_auc_score
 import torch
+import torch.nn.functional as F
 from torchnlp.metrics import get_accuracy
 from core.utils.configuration import configmapper
 
@@ -10,7 +11,7 @@ def binary_auroc(outputs,labels):
     Parameters
     ----------
     outputs: torch.Tensor
-        Tensor containing the softmax outputs from the model
+        Tensor containing the logit outputs from the model
     labels: torch.Tensor
         Tensor containing the labels (Not one-hot encoded)
 
@@ -23,7 +24,7 @@ def binary_auroc(outputs,labels):
     ## More functionality can be added through the parameters.
     ## Custom AUROC function/class can also be defined.
     """
-    outputs_index = outputs[:,1]
+    outputs_index = F.softmax(outputs,dim=1)[:,1]
     return roc_auc_score(labels.detach().numpy(),outputs_index.detach().numpy())
 
 @configmapper.map('metrics','accuracy')
@@ -33,7 +34,7 @@ def accuracy(outputs,labels):
     Parameters
     ----------
     outputs: torch.Tensor
-        Tensor containing the softmax outputs from the model
+        Tensor containing the logit outputs from the model
     labels: torch.Tensor
         Tensor containing the labels (Not one-hot encoded)
 
@@ -46,5 +47,5 @@ def accuracy(outputs,labels):
     ## More functionality can be added through the parameters.
     ## Custom function/class can also be defined.
     """
-    outputs_argmax = torch.argmax(outputs,dim=1)
+    outputs_argmax = torch.argmax(F.softmax(outputs.detach(),dim=1),dim=1)
     return get_accuracy(labels,outputs_argmax)[0]